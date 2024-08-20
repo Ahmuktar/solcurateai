2c29c6b4c7c405f5ceedda4e7df6478f72eb2169
Ahmuktar <ahmuktar203@gmail.com>
2024-08-20 11:38:36 +0100

    first commit

---
+++ constants/index.js
@@ -0,0 +1,102 @@
+export const websiteLinks = [
+    {
+        label: 'Home',
+        route: '/',
+    },
+    {
+        label: 'Features',
+        route: '#features',
+    },
+    {
+        label: 'How it works',
+        route: '#howitworks',
+    },
+]
+
+export const sidebarLinks = [
+    {
+        label: 'Dashboard',
+        route: '',
+        imgUrl: '/icons/home.svg',
+    },
+    {
+        label: 'Search',
+        route: '/search',
+        imgUrl: '/icons/search.svg',
+    },
+    {
+        label: 'Content Creation',
+        route: '/content',
+        imgUrl: '/icons/ai-book.svg',
+    },
+    {
+        label: 'Solana Community',
+        route: '/solana-community',
+        imgUrl: '/icons/solana.svg',
+    },
+    {
+        label: 'AI Editor',
+        route: '/ai-editor',
+        imgUrl: '/icons/input-ai.svg',
+    },
+    {
+        label: 'Audience Interaction',
+        route: '/audience-interaction',
+        imgUrl: '/icons/touch.svg',
+    },
+    {
+        label: 'Brand Tailoring',
+        route: '/brand-tailoring',
+        imgUrl: '/icons/settings-2.svg',
+    },
+    {
+        label: 'Strategy Crafting',
+        route: '/strategy',
+        imgUrl: '/icons/create.svg',
+    },
+    {
+        label: 'Platform Setup',
+        route: '/platform',
+        imgUrl: '/icons/platform.svg',
+    },
+    {
+        label: 'Media Creation',
+        route: '/media',
+        imgUrl: '/icons/photo-ai.svg',
+    },
+    {
+        label: 'Settings',
+        route: '/settings',
+        imgUrl: '/icons/settings.svg',
+    },
+]
+
+
+export const settingLinks = [
+    {
+        label: 'Edit Profile',
+        route: '',
+        imgUrl: '/icons/user.svg',
+    },
+    {
+        label: 'Password',
+        route: '/password',
+        imgUrl: '/icons/keyhole.svg',
+    },
+    {
+        label: 'Notifications',
+        route: '/notifications',
+        imgUrl: '/icons/bell.svg',
+    },
+    {
+        label: 'Wallets',
+        route: '/wallets',
+        imgUrl: '/icons/container.svg',
+    },
+    {
+        label: 'Appearance',
+        route: '/appearance',
+        imgUrl: '/icons/sun.svg',
+    },
+    
+]
